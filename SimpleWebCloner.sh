69dbd4b3c936fc489d615d1e450938a255b5ae57
Josh <jlwarren1999@gmail.com>
2022-05-03 11:41:44 +0800

    Update SimpleWebCloner.sh

--- SimpleWebCloner.sh
+++ SimpleWebCloner.sh
@@ -1,5 +1,5 @@
 # OSMANiC SimpleWebCloner Copyright 2022
-# Version 1.3.0
+# Version 1.3.2
 # This is free software, obtain a copy at https://github.com/OSMANiCTeam/SimpleWebCloner
 # If you paid for this software, demand a refund
 
@@ -15,7 +15,7 @@ wget --mirror            \
      --html-extension    \
      --wait=2            \
      -o log              \
-     --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"	\
+     --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36"	\
      $web_address
 echo The website has been saved
 echo Simple Web Cloner CLI tool by OSMANiC Team, powered by wget
